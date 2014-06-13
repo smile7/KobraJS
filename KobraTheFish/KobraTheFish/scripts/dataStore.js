cb26bbf5844e56561c411429c08cab077a2faa1f
dimitar-mitev <erty.mitev@gmail.com>
2014-06-13 19:37:42 +0300

    localstore/cookies
    base for storing data clientside so we can restore a game state in a new
    session

---
+++ KobraTheFish/KobraTheFish/scripts/dataStore.js
@@ -0,0 +1,34 @@
+﻿function saveSession() {
+    if (typeof (Storage) !== "undefined") {
+        saveToLocalStorage();
+    } else {
+        saveToCookies();
+    }
+}
+
+function loadSession() {
+    if (typeof (Storage) !== "undefined") {
+        loadFromLocalStorage();
+    } else {
+        loadFromCookies();
+    }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("userName", 'test');
+    //and so on need to check whats what
+}
+
+function saveToCookies() {
+    var cookiesString;
+    cookiesString = "" + "username=test";
+    //and so on need to see whats what
+    document.cookie = cookiesString;
+}
+
+function loadFromLocalStorage() {
+    //work in progress, might change function all together
+}
+function loadFromCookies() {
+    //work in progress, might change function all together
+}
