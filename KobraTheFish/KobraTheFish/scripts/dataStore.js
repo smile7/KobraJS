884fb28771b88974b36366dc32160aeb0b529805
stefanNaumov <stefannaumow@gmail.com>
2014-06-14 23:46:48 +0300

    .

---
+++ KobraTheFish/KobraTheFish/scripts/dataStore.js
@@ -0,0 +1,35 @@
+﻿function saveSession() {
+    if (typeof (Storage) !== "undefined") {
+        saveToLocalStorage();
+    } else {
+        saveToCookies();
+    }
+}
+
+function loadSession() {
+    if (typeof (Storage) !== "undefined") {
+        loadFromLocalStorage();
+    } else {
+        loadFromCookies();
+    }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("userName", 'test');
+    //and so on need to check whats what
+}
+
+function saveToCookies() {
+    var cookiesString;
+    cookiesString=
+    cookiesString = "" + "username=test";
+    //and so on need to see whats what
+    document.cookie = cookiesString;
+}
+
+function loadFromLocalStorage() {
+    //work in progress, might change function all together
+}
+function loadFromCookies() {
+    //work in progress, might change function all together
+}
